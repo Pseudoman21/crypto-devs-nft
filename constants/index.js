fa985c7a7b407042f44fd5a4718bbf327fda2e83
John Paul M. Batusan <johnpaul.batusa@bicol-u.edu.ph>
2022-12-08 20:02:02 +0800

    Add constant root file

---
+++ constants/index.js
@@ -0,0 +1,22 @@
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address'
+      }
+    ],
+    name: 'whitelistedAddresses',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool'
+      }
+    ],
+    stateMutability: 'view',
+    type: 'function'
+  }
+]
+export const NFT_CONTRACT_ADDRESS = '0x74757EA27657a202cF62c1adFb3d994F80070053'
